ed17a5cab5fdbc7669d5ea0fc64a76dba9a125c1
Eldon Hipolito <eldon.hipolito@pointwest.com.ph>
2018-04-04 11:06:50 +0800

    Refactored merge sort

--- src/github.com/eldonhipolito/MergeSort/MergeSort.go
+++ src/github.com/eldonhipolito/MergeSort/MergeSort.go
@@ -1,75 +1,66 @@
 package main
 
-
 import "fmt"
 
-type ArrayInt []int 
-
+type ArrayInt []int
 
 type Sortable interface {
 	Sort(asc bool)
 }
 
-func MergeSort(arr []int, p,q,r int, asc bool) {
-	if((r - p) == 0) {
-	//fmt.Printf("Ret : P : %v - Q: %v - R: %v\n", p, q, r)
+func MergeSort(arr, temp []int, p, r int, asc bool) {
+	if p == r {
 		return
 	}
 
 	//Divide until base is 1
-//fmt.Printf("P : %v - Q: %v - R: %v\n", p, q, r)
-	MergeSort(arr, p, (p + q) / 2 , q, asc)
-	MergeSort(arr, q + 1, (r + q + 1)/ 2, r, asc)
+	q := (r + p) / 2
+	MergeSort(arr, temp, p, q, asc)
+	MergeSort(arr, temp, q+1, r, asc)
 
-	subArr := make([]int, (r + 1) - p)
-	i,i2,sub := 0,0,0
-	for ; (i + p) <= q && (q + 1 + i2) <= r; sub++ {
-		//fmt.Printf("arr[i + p] : %v - arr[q + 1 + i2] : %v\n",arr[i + p], arr[q + 1 + i2])
-		if(arr[i + p] > arr[q + 1 + i2] == asc) {
-			subArr[sub] = arr[q + 1 + i2]
+	subArr := temp[:r+1]
+	i, i2, sub := 0, 0, 0
+	for ; (i+p) <= q && (q+1+i2) <= r; sub++ {
+		if arr[i+p] > arr[q+1+i2] == asc {
+			subArr[sub] = arr[q+1+i2]
 			i2++
 		} else {
-			subArr[sub] = arr[i + p]
+			subArr[sub] = arr[i+p]
 			i++
 		}
 	}
 
 	for ; (i + p) <= q; i++ {
-		subArr[sub] = arr[i + p]
+		subArr[sub] = arr[i+p]
 		sub++
 
 	}
 	for ; (q + 1 + i2) <= r; i2++ {
-		subArr[sub] = arr[ q + 1 + i2]
+		subArr[sub] = arr[q+1+i2]
 		sub++
 
 	}
 
-	//fmt.Printf("Subarr[] : %v\n", subArr)	
-	copy(arr[p:r + 1], subArr)
-	
+	copy(arr[p:r+1], subArr)
 
-	
 }
 
 func (arr ArrayInt) Sort(asc bool) {
-		MergeSort(arr, 0, len(arr) / 2 , len(arr) - 1, asc)
+	MergeSort(arr, make([]int, len(arr)), 0, len(arr)-1, asc)
 
-	
 }
-
 
 func main() {
 	var a Sortable
 
-	a = ArrayInt([]int{1,8,3,2,5})
+	a = ArrayInt([]int{1, 8, 3, 2, 5})
 
 	//ASC
 	fmt.Printf("Prior to sort : %v\n", a)
 	a.Sort(true)
 	fmt.Printf("After sort : %v\n", a)
 
-	a = ArrayInt([]int{6,4,3,2,1,5,6,3,11})
+	a = ArrayInt([]int{6, 4, 3, 2, 1, 5, 6, 3, 11, 711})
 
 	fmt.Printf("Prior to sort : %v\n", a)
 	a.Sort(false)
